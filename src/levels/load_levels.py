7d400c800a6b1100846711b51ac272945e4d2808
Javier Santos <javsanmar5@alum.us.es>
2025-02-04 00:30:27 +0100

    feat: Extract levels into a separate file Closes #5
    Co-authored-by: darrodsas <darrodsas@alum.us.es>

---
+++ src/levels/load_levels.py
@@ -0,0 +1,13 @@
+import os
+import glob
+
+def load_levels(directory="src/levels"):
+    levels = {}
+    for filepath in glob.glob(os.path.join(directory, "*.txt")):
+        with open(filepath, "r") as file:
+            level_layout = [line.rstrip("\n") for line in file if line.strip()]
+        filename = os.path.splitext(os.path.basename(filepath))[0]
+        level_name = filename.capitalize()   
+        levels[level_name] = level_layout
+    return levels
+
