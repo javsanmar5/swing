05ae7d8ca42635afd2132688d1d7f8d3842bf72b
Javier Santos <javsanmar5@alum.us.es>
2025-02-04 15:44:18 +0100

    feat: Add winning tile Refs #4

--- src/Level.py
+++ src/Level.py
@@ -8,6 +8,7 @@ class Level:
         self.cols = len(layout[0]) if self.rows > 0 else 0
         self.players = []
         self.obstacles = []
+        self.win = None
         self.parse_layout()
 
     def parse_layout(self):
@@ -21,24 +22,33 @@ class Level:
                     self.players.append(player)
                 elif tile == "X":
                     self.obstacles.append((x, y))
+                elif tile == "W":
+                    self.win = (x, y)
 
     def draw(self, surface):
         width, height = surface.get_size()
         tile_w = width / self.cols
         tile_h = height / self.rows
+
         for y, row in enumerate(self.layout):
             for x, tile in enumerate(row):
                 rect = pygame.Rect(x * tile_w, y * tile_h, tile_w, tile_h)
                 color = (100, 100, 100) if (x + y) % 2 == 0 else (120, 120, 120)
                 pygame.draw.rect(surface, color, rect)
+
         for player in self.players:
             x, y = player.x, player.y 
             rect = pygame.Rect(x * tile_w, y * tile_h, tile_w, tile_h)
             pygame.draw.rect(surface, player.color ,rect)
+        
         for (x, y) in self.obstacles:
             rect = pygame.Rect(x * tile_w, y * tile_h, tile_w, tile_h)
             pygame.draw.rect(surface, "black", rect)
-       
+
+        if self.win: 
+            win_rect = pygame.Rect(self.win[0] * tile_w, self.win[1] * tile_h, tile_w, tile_h)
+            pygame.draw.rect(surface, "bisque", win_rect) 
+
     def update(self, screen, dt):
         keys = pygame.key.get_pressed()
         for player in self.players:
