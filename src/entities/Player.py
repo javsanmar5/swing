3fa827d78fb0fd6ae5f1da4df31036e2f889ab19
Javier Santos <javsanmar5@alum.us.es>
2025-02-05 18:21:58 +0100

    feat: Add door/keys feature Closes #15

--- src/entities/Player.py
+++ src/entities/Player.py
@@ -6,8 +6,10 @@ class Player:
         self.y = y
         self.color = color
         self.locked_direction = None
-
-    def update(self, keys, cols, rows, obstacles):
+        self.open_keys = 0
+        self.to_open_door = False
+    
+    def update(self, keys, cols, rows, obstacles, doors, open_keys):
         if self.locked_direction is None:
             if keys[pygame.K_LEFT]:
                 self.locked_direction = (-1, 0)
@@ -23,8 +25,19 @@ class Player:
             new_x = self.x + dx
             new_y = self.y + dy
 
-            if (new_x, new_y) in obstacles:
+            if (new_x, new_y) in open_keys:
+                open_keys.remove((new_x, new_y))
+                self.open_keys += 1
+
+            if (new_x, new_y) in obstacles or (new_x, new_y) in doors:
                 self.locked_direction = None
+                if (new_x, new_y) in doors and self.open_keys >= 1:
+                    if self.to_open_door:
+                        doors.remove((new_x, new_y))
+                        self.open_keys -= 1
+                        self.open_door = False
+                    else:
+                        self.to_open_door = True
                 return
 
             if new_x < 0 or new_x >= cols or new_y < 0 or new_y >= rows:
